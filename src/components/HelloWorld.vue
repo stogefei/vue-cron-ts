e4e53b012397fb72e252fb72a34e91c27ddd85cc
zhuaf <zhuaf@authine.com>
2019-12-04 15:51:57 +0800

    添加校验测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="hello">
-    <Cron v-model="cronValue"/>
+    <Cron v-model="cronValue" />
     <div style="width:70%;margin:0 auto">
       <h3>检验表达式</h3>
-      <a-input placeholder='输入表达式' v-model="cronValue2"></a-input>
-      <br/>
-      <br/>
-      <a-button type='primary' @click="test" block>检验</a-button>
+      <a-input placeholder="输入表达式" v-model="cronValue2"></a-input>
+      <br />
+      <br />
+      <a-button type="primary" @click="test" block>检验</a-button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import Cron from "./cron/index.vue";
 @Component({
   name: "HelloWorld",
@@ -24,12 +24,16 @@ export default class HelloWorld extends Vue {
   public cronValue: any = "";
   public cronValue2: any = "";
 
-  test() {
-    this.cronValue = this.cronValue2
+  public test() {
+    this.cronValue = this.cronValue2;
+  }
+
+  @Watch("cronValue")
+  public cronValue_Change(v: any) {
+    this.cronValue2 = v;
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
-</style>
+<style scoped lang="less"></style>
